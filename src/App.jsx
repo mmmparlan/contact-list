5380f069edf6f39652e4e1ffec916f45ff2ee7a8
mmmparlan <mmparlan@gmail.com>
2023-07-26 15:09:26 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+
+  },[]);
+
+  return (
+    <>
+      
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        
+    </>
+  )
+}
+
+export default App
